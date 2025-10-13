6967cab95258640f69aa4fd79bdc2063873a1c63
xierongsheng <447618716@qq.com>
2025-10-13 16:30:44 +0800

    refactor: 更新交集观察者回调参数名称并检查 null

--- src/directive/vLazy.ts
+++ src/directive/vLazy.ts
@@ -4,8 +4,8 @@ import type { Directive } from 'vue';
 const vLazy: Directive<HTMLImageElement, string> = {
     mounted(el, binding) {
         el.src = `https://dummyimage.com/${el.width}x${el.height}/e0e0e0/333333&text=加载中...`; // 默认占位图
-        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-            if (isIntersecting) {
+        const { stop } = useIntersectionObserver(el, ([intersectionObserver]) => {
+            if (intersectionObserver?.isIntersecting) {
                 el.src = binding.value; // 进入视口加载真实图片
                 stop();
             }
